f7418b669a92547bc168ea09c0ef5f63d0693631
arthurdemurger <arthurdemurger26@gmail.com>
2023-09-26 12:49:37 +0200

    working inception

--- src/requirements/mariadb/tools/install_mariadb.sh
+++ src/requirements/mariadb/tools/install_mariadb.sh
@@ -1,18 +1,18 @@
 # Check if the specified database already exists
-if [ -d /var/lib/mysql/$MARIADB_NAME ]; then
-	echo "DB $MARIADB_NAME already exists..."
+if [ -d /var/lib/mysql/$SQL_DATABASE ]; then
+	echo "DB $SQL_DATABASE already exists..."
 else
 	# Start the MySQL service with the specified data directory
 	service mysql start --datadir=/var/lib/mysql
 
 	# Create the database and root user using the provided SQL script
 	echo "create database if not exists $SQL_DATABASE;"| mysql -u root
-	echo "create user '$SQL_ADMIN_USER'@'%' identified by '$SQL_ROOT_PASSWORD';" | mysql -u root
-	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_ADMIN_USER'@'%';" | mysql -u root
+	echo "create user '$SQL_ADMIN'@'%' identified by '$SQL_PWD';" | mysql -u root
+	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_ADMIN'@'%';" | mysql -u root
 	echo "FLUSH PRIVILEGES;" | mariadb -u root
 
 	# Set a password for the root user
-	mysqladmin -u root password $WP_ADMIN_PWD
+	mysqladmin -u root password $SQL_PWD
 
 	# Stop the MySQL service
 	service mysql stop --datadir=/var/lib/mysql
--- src/requirements/wordpress/tools/wp_installer.sh
+++ src/requirements/wordpress/tools/wp_installer.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+sleep 5
 # Configure PHP-FPM pour écouter sur le port 9000 au lieu d'un socket
 sed -i "s|listen = /run/php/php7.3-fpm.sock|listen = 9000|g" /etc/php/7.3/fpm/pool.d/www.conf
 
 # Vérifie si WordPress est déjà installé
+
+ls -la
+
 if [ ! -f ./wp-config.php ]; then
     # Télécharge WordPress
     wp core download --allow-root --path=/var/www/wordpress/
 
     # Crée le fichier de configuration de WordPress
-    wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$ADMIN_USER --dbpass=$WP_ADMIN_PWD \
+    wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_ADMIN --dbpass=$SQL_PWD \
         --dbhost=$SQL_HOST --path='/var/www/wordpress'
 
     # Installe WordPress
-    wp core install --allow-root --title=$SITE_TITLE --url=$SITE_URL \
-        --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL
+    wp core install --allow-root --title=$WP_TITLE --url=$WP_URL \
+        --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL
 
     # Crée un nouvel utilisateur
-    wp user create --allow-root $USER1_LOGIN $USER1_EMAIL --user_pass=$USER1_PASSWORD
+    wp user create --allow-root $WP_USER1 $WP_USER1_MAIL --user_pass=$WP_USER1_PWD
 
 else
     echo "Wordpress is already installed and configured"
